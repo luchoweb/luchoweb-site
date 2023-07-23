02547e67c8fac3f08c4139ae57981380bb1aba0c
Lucho Web <luchoweb.dev@gmail.com>
2023-07-23 18:22:32 -0500

    Language small fix

--- src/components/Languages/index.tsx
+++ src/components/Languages/index.tsx
@@ -7,7 +7,7 @@ const Languages = () => {
     <ul className="languages__list list-unstyled m-0 p-0 d-flex gap-3">
       {languages.map((lang) => (
         <li key={lang.name} className="lang simple-card">
-          <p className="m-0 lang__name">{lang.name}</p>
+          <p className="lang__name">{lang.name}</p>
           <p className="m-0 lang__level">{lang.level}</p>
         </li>
       ))}
