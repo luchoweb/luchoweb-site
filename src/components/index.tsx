524b1506237897a25acc6dd18626eeb1906d0280
Lucho Web <luchoweb.dev@gmail.com>
2024-10-08 08:49:20 -0500

    Dev to Main (#1)

--- src/components/index.tsx
+++
@@ -1,8 +0,0 @@
-export { default as Section } from "./Section";
-export { default as Resume } from "./Resume";
-export { default as Profile } from "./Profile";
-export { default as Skills } from "./Skills";
-export { default as Languages } from "./Languages";
-export { default as Information } from "./Information";
-export { default as Experience } from "./Experience";
-export { default as Education } from "./Education";
