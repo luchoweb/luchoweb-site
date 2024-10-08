a08e547e4c443f9632a0c46350a6c9ed17bb7c28
Lucho Web <luchoweb.dev@gmail.com>
2024-10-08 09:23:44 -0500

    relative path removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,3 @@
 import { defineConfig } from 'astro/config';
-import relativeLinks from 'astro-relative-links';
 
-export default defineConfig({
-  integrations: [relativeLinks()],
-});
+export default defineConfig({});
